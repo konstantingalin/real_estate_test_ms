640795301f9af46bc728b2b084e426492150803c
Konstantin <38990885+konstantingalin@users.noreply.github.com>
2023-01-25 12:17:53 +0600

    add_progress_bar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -26,4 +26,51 @@ window.addEventListener('DOMContentLoaded', function () {
 		selectSingle.setAttribute('data-state', '');
 	});
 	}
+
+	// Progress bar
+
+	const prosress_bar_item = document.querySelector('.progress_bar__item p');
+	const prosress_bar_num = document.querySelector('.progress_bar__item .num');
+	const prosress_bar_button = document.querySelector('.progress_bar__box__item .button');
+	let num = prosress_bar_num.textContent * 1;
+
+	function numPlus (num) {
+		if(num < 25) {
+			setTimeout(() => {
+				num++;
+				prosress_bar_num.textContent = num;
+				numPlus(num);
+			}, 35);
+		} else {
+			prosress_bar_button.classList.add('none');
+		}
+	}
+
+	prosress_bar_button.addEventListener('click', (e) => {
+		e.preventDefault();
+
+		if(!prosress_bar_button.classList.contains('none')) {
+			numPlus(num);
+		}
+		
+		prosress_bar_item.style.width = '35%';
+	})
+
+
+	// Accordion
+
+	const accordion_button = document.querySelector('.accordion_button');
+	const accordion_box = document.querySelector('.accordion_box');
+
+	accordion_button.addEventListener('click', e => {
+		e.preventDefault();
+
+		if(accordion_button.classList.contains('active')){
+			accordion_box.style.display = 'none';
+			accordion_button.classList.remove('active');
+		} else {
+			accordion_box.style.display = 'block';
+			accordion_button.classList.add('active');
+		}
+	})
 });
